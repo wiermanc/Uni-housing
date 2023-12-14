cf0de5b7a8c5d583656a80e65ea4be27c24a4725
wiermanc <98360544+wiermanc@users.noreply.github.com>
2023-12-14 00:53:25 -0700

    Update ListingCard.js

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './ListingCard.css'; // Ensure this is the correct path to your CSS file
+import './ListingCard.css'; // Make sure this path is correct for your CSS file
 
 function ListingCard({ listing }) {
   let navigate = useNavigate();
 
+  // Function to navigate to the detail page of the listing when the card is clicked
   const goToDetailPage = () => {
     navigate(`/listing/${listing.id}`);
   };
 
+  // Calculates and formats the price range for display on the card
+  // If it's an individual room, show the range, otherwise, show the fixed price
   const getPriceRange = (listing) => {
     if (listing.propertyType === 'Individual Room' && listing.availableBedrooms.length) {
       const openRooms = listing.availableBedrooms.filter(room => room.status === 'Open');
@@ -26,15 +29,21 @@ function ListingCard({ listing }) {
   return (
     <div className="card" onClick={goToDetailPage}>
       <div className="card-image">
+        {/* Display the first image of the listing */}
         {firstImage && <img src={firstImage} alt={listing.title} />}
         <div className="property-type-label">
+          {/* Label indicating if the listing is for a full property or an individual room */}
           {listing.propertyType === 'Full Property' ? 'Full Property' : 'Room'}
         </div>
       </div>
       <div className="card-content">
+        {/* Display the price or price range of the listing */}
         <div className="card-price">{priceRange}</div>
+        {/* Display the title of the listing */}
         <h3 className="card-title">{listing.title}</h3>
+        {/* Display the address if they say to */}
         {listing.displayAddress === 'Yes' && <p className="card-address">{listing.address}</p>}
+        {/* Display the description of the listing */}
         <p className="card-description">{listing.description}</p>
       </div>
     </div>
@@ -42,3 +51,4 @@ function ListingCard({ listing }) {
 }
 
 export default ListingCard;
+
